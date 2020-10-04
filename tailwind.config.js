a403c85f0b7854f88ef4d728ac180c0ba47d0b8e
movedx <grytsenko.bogdan@gmail.com>
2020-10-04 19:16:43 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  purge: [
+    "src/**/*.js",
+    "src/**/*.jsx",
+    "src/**/*.ts",
+    "src/**/*.tsx",
+    "public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
