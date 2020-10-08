8c80e1cbb8864dbb20b1c3686aa585ea8370f7cf
movedx <grytsenko.bogdan@gmail.com>
2020-10-08 16:11:46 +0200

    add bank1 and keyboard support

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as Tone from "tone";
 import "../tailwind.output.css";
 import banks from "./banks";
 
 const Pad = (props) => {
-  const bank = "b1";
+  let bank = "b1";
   const sample = banks[bank][props.id[1]];
-  console.log(sample, bank, props.id);
   const player = new Tone.Player(sample).toDestination();
+
   const play = async () => {
     await Tone.loaded();
     player.start();
   };
+  const handleKeyDown = async (event) => {
+    if (event.key === props.id[1]) {
+      play();
+    }
+  };
+  useEffect((props) => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
-    <div
-      onClick={play}
-      className="border border-solid border-1 w-16 h-16"
-    ></div>
+    <button onClick={play} className="border border-solid border-1 w-16 h-16">
+      {props.id[1]}
+    </button>
   );
 };
 
